a28e5159476a2cf0b574812888a77fb4cfe29780
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2018-04-03 16:28:55 +0530

    shared for day

--- app/src/main/java/org/viciousanddelicious/viciousdelicious/day_order.java
+++ app/src/main/java/org/viciousanddelicious/viciousdelicious/day_order.java
@@ -1,7 +1,13 @@
 package org.viciousanddelicious.viciousdelicious;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import static org.viciousanddelicious.viciousdelicious.timetable.preference_day;
+import static org.viciousanddelicious.viciousdelicious.timetable.saveit_day;
 
 public class day_order extends AppCompatActivity {
 
@@ -9,5 +15,8 @@ public class day_order extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_order);
+        SharedPreferences sf3=getSharedPreferences(preference_day, Context.MODE_PRIVATE);
+        String book_cover = sf3.getString(saveit_day,"");
+        Toast.makeText(this, book_cover, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/org/viciousanddelicious/viciousdelicious/timetable.java
+++ app/src/main/java/org/viciousanddelicious/viciousdelicious/timetable.java
@@ -1,6 +1,10 @@
 package org.viciousanddelicious.viciousdelicious;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -11,11 +15,19 @@ import com.hitomi.cmlibrary.OnMenuSelectedListener;
 public class timetable extends AppCompatActivity {
 
     String ArrayName[]={"Day order 1","Day order 2","Day order 3","Day order 4","Day order 5"};
+    public static final String preference_day="pref_day";
+    public static final String saveit_day="savekey_day";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
+        SharedPreferences sf_day=getSharedPreferences(preference_day, Context.MODE_PRIVATE);
+
+
+     final   SharedPreferences.Editor editor2 = sf_day.edit();
+
+
 
         CircleMenu circlemenu = (CircleMenu) findViewById(R.id.circle_menu_timetable);
         circlemenu.setMainMenu(Color.parseColor("#ffffff"),R.drawable.timetable,R.drawable.multiply)
@@ -27,7 +39,42 @@ public class timetable extends AppCompatActivity {
                 .setOnMenuSelectedListener(new OnMenuSelectedListener() {
                     @Override
                     public void onMenuSelected(int i) {
-                        Toast.makeText(timetable.this, "Hello", Toast.LENGTH_SHORT).show();
+                        final int x=i;
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                        if(x==0)
+                        {
+                            editor2.putString(saveit_day,Integer.toString(x+1) );
+                            editor2.commit();
+
+                        }
+                        if(x==1)
+                        {
+
+                            editor2.putString(saveit_day,Integer.toString(x+1) );
+                            editor2.commit();
+                        }
+                        if(x==2)
+                        {
+                            editor2.putString(saveit_day,Integer.toString(x+1) );
+                            editor2.commit();
+                        }
+                        if(x==3)
+                        {
+                            editor2.putString(saveit_day,Integer.toString(x+1) );
+                            editor2.commit();
+                        }
+                        if(x==4)
+                        {
+                            editor2.putString(saveit_day,Integer.toString(x+1) );
+                            editor2.commit();
+                        }
+                        startActivity(new Intent(timetable.this,day_order.class));
+                            }
+                        },1000);
+
                     }
                 });
     }
