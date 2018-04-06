9b8fbd6f13b5f39fbff58447d38f40d785c63b58
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2018-04-06 06:08:56 +0530

    Changes

--- app/src/main/java/org/viciousanddelicious/viciousdelicious/timetable.java
+++ app/src/main/java/org/viciousanddelicious/viciousdelicious/timetable.java
@@ -1,9 +1,11 @@
 package org.viciousanddelicious.viciousdelicious;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,15 +14,24 @@ import android.widget.Toast;
 import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class timetable extends AppCompatActivity {
 
     String ArrayName[]={"Day order 1","Day order 2","Day order 3","Day order 4","Day order 5"};
     public static final String preference_day="pref_day";
     public static final String saveit_day="savekey_day";
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        pd = ProgressDialog.show(timetable.this, "", "Please wait...", true);
         setContentView(R.layout.activity_timetable);
         SharedPreferences sf_day=getSharedPreferences(preference_day, Context.MODE_PRIVATE);
 
@@ -77,5 +88,91 @@ public class timetable extends AppCompatActivity {
 
                     }
                 });
+        new fetchery().execute();
+    }
+    class fetchery extends AsyncTask<Void,Void,Void> {
+        private Document doc=null;
+        Element table=null;
+        Elements temp=null;
+        Element row=null;
+        Elements cols=null;
+        @Override
+        protected Void doInBackground(Void... arg0) {
+     /*   try {
+            Connection.Response res = Jsoup
+                    .connect("http://evarsity.srmuniv.ac.in/srmswi/usermanager/ParentLogin.jsp")
+                    .data("txtRegNumber", "iamalsouser")
+                    .data("txtPwd", "thanksandregards")
+                    .data("txtSN", "PRA1611003040056")
+
+                    .data("txtPD", "07061998")
+                    .data("txtPA", "1")
+                    .method(Connection.Method.GET)
+                    .execute();
+
+            //Get cookies
+            Map<String, String> cookies = res.cookies();
+
+            doc = Jsoup.connect("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=5").cookies(cookies).get();
+            table = doc.select("table").get(0); //select the first table.
+
+            temp = table.select("tr");
+
+            row = temp.get(1);
+            cols = row.select("td");
+        } catch (Exception e) {
+            System.err.println(e);
+
+            startActivity(new Intent(day_order.this,timetable.class));
+
+        }*/
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            String str="15CS204J,15CS204J 15CS205,15CS205";
+            List<String> list=new ArrayList<String>();
+
+      /*      if (cols!=null) {
+
+
+                for (int i = 0; i < cols.size(); i++)
+                {
+                    list.add(cols.get(i).text());
+                System.out.println(cols.get(i).text());
+                }
+
+                System.out.println(list);
+            }*/
+
+
+      /*
+          import java.util.*;
+public class HelloWorld{
+
+     public static void main(String []args){
+
+         String str="15CS204J, 15CS203, 15CS205, 15CS205";
+         Set<String> hash = new HashSet<String>();
+          List<String> list=new ArrayList<String>();
+          int k=0;
+         String[] s=str.split(",");
+         String s2;
+         for(int i=0;i<s.length;i++)
+         {
+             s2=s[i].trim();
+             hash.add(s2);
+         }
+         list.addAll(hash);
+        System.out.println(list);
+     }
+} 
+       */
+
+            pd.dismiss();
+
+        }
+
     }
 }
